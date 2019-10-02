711e57abc6d046d9edf2e009e291cc7c59d68ba3
Andrés Bravo <sbravir642elpuig.xeill.net>
2019-10-02 23:59:51 +0200

    actividad terminada

--- src/main/java/Dau.java
+++ src/main/java/Dau.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public class Dau {
     Random random = new Random();
-    Scanner scanner = new Scanner(System.in);
-    int numVeces;
-    int dados[] = new int[numVeces];
     int dado;
 
     public void tirardados(){
--- src/main/java/JocdeDaus.java
+++ src/main/java/JocdeDaus.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class JocdeDaus {
     Random random = new Random();
     Scanner scanner = new Scanner(System.in);
+    int countGanados=0, countPerdidos=0;
     Dau dau1;
     Dau dau2;
     Dau dau3;
@@ -20,17 +21,30 @@ public class JocdeDaus {
         dau2.tirardados();
         dau3.tirardados();
 
+        System.out.print("Dau 1 --> ");
         dau1.imprimir();
+        System.out.print(" Dau 2 --> ");
         dau2.imprimir();
+        System.out.print(" Dau 3 --> ");
         dau3.imprimir();
 
         System.out.println();
 
         if(dau1.dado==dau2.dado & dau2.dado==dau3.dado){
-            System.out.println("GANADO");
-            return;
-        }
+            System.out.println("¡¡HAS GANADO!!");
+            countGanados++;
 
+        } else{ countPerdidos++; }
+
+    }
+
+    void gestionPartidas(){
+        System.out.println("HAS PERDIDO: " + countPerdidos + " VECES");
+        if(countGanados==1){
+            System.out.println("HAS GANADO: " + countGanados + " VEZ");
+        } else {
+            System.out.println("HAS GANADO: " + countGanados + " VECES");
+        }
     }
 
 }
